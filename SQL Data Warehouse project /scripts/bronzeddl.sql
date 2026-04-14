40348ffa266a4075a8b51be7f0d03b80c98e2a53
Benjan Adhikari <99618883+beadhika@users.noreply.github.com>
2026-04-14 10:26:32 -0600

    Update bronzeddl.sql

--- SQL Data Warehouse project /scripts/bronzeddl.sql
+++ SQL Data Warehouse project /scripts/bronzeddl.sql
@@ -1,3 +1,6 @@
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+
 CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),            
@@ -8,6 +11,9 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date DATE
 );
 
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info;
+
 CREATE TABLE bronze.crm_prd_info (
     prd_id INT,
     prd_key NVARCHAR(50),
@@ -17,6 +23,9 @@ CREATE TABLE bronze.crm_prd_info (
     prd_start_dt DATETIME,
     prd_end_dt DATETIME
 );
+
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details;
 
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num NVARCHAR(50),
@@ -30,11 +39,16 @@ CREATE TABLE bronze.crm_sales_details (
     sls_price INT
 );
 
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101;
 
 CREATE TABLE bronze.erp_loc_a101 (
     cid NVARCHAR(50),
     cntry NVARCHAR(50)
 );
+
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12;
 
 CREATE TABLE bronze.erp_cust_az12 (
     cid NVARCHAR(50),
@@ -42,6 +56,9 @@ CREATE TABLE bronze.erp_cust_az12 (
     gen NVARCHAR(50)
 );
 
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2;
+    
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id NVARCHAR(50),
     cat NVARCHAR(50),
