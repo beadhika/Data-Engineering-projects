1c5e99121d9f814f1110381755ddc3dddd746851
Benjan Adhikari <99618883+beadhika@users.noreply.github.com>
2026-04-22 14:13:16 -0600

    Update and rename readingfiles to proc_load_bronze.sql

--- SQL Data Warehouse project /scripts/proc_load_bronze.sql
+++ SQL Data Warehouse project /scripts/proc_load_bronze.sql
@@ -20,11 +20,46 @@ WITH (
 
 
 -- 4) Load data into bronze table from CSV file
+TRUNCATE TABLE bronze.crm_cust_info;
+
 BULK INSERT bronze.crm_cust_info
 FROM 'crmdata/cust_info.csv'
 WITH (
-    DATA_SOURCE = 'CrmBlob',
+    DATA_SOURCE = 'MyBlobStorage',
     FIRSTROW = 2,
     FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '0x0a'
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_prd_info;
+
+BULK INSERT bronze.crm_prd_info
+FROM 'crmdata/prd_info.csv'
+WITH (
+    DATA_SOURCE = 'MyBlobStorage',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.crm_sales_details;
+
+BULK INSERT bronze.crm_sales_details
+FROM 'crmdata/sales_details.csv'
+WITH (
+    DATA_SOURCE = 'MyBlobStorage',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
+);
+
+TRUNCATE TABLE bronze.erp_cust_az12;
+
+BULK INSERT bronze.erp_cust_az12
+FROM 'erpdata/CUST_AZ12.csv'
+WITH (
+    DATA_SOURCE = 'MyBlobStorage',
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    TABLOCK
 );
